db1224a1ab6a3894a4835a4dc06dbf4a329548e0
Eduard García <eddvidya@hotmail.com>
2021-07-07 13:51:51 -0400

    Configuración para DRF

--- hotel/urls.py
+++ hotel/urls.py
@@ -13,9 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from api import views
+
+router = routers.DefaultRouter()
+router.register(r'clients', views.ClientViewSet)
+router.register(r'rooms', views.RoomViewSet)
+router.register(r'reserves', views.ReserveViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
